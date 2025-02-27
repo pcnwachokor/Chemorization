6ccc605dea13806652086e0b4d0b07e15be5e9af
Isaiah <maberry884@gmail.com>
2025-02-27 12:54:48 -0600

    implemented text to braille translation and pdf generation

--- Chemorization-App/app/(tabs)/four.tsx
+++ Chemorization-App/app/(tabs)/four.tsx
@@ -1,8 +1,15 @@
-import { StyleSheet } from 'react-native';
-
+/*import { StyleSheet } from 'react-native';
+import { Keyboard } from 'react-native'; */
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import React, { useState } from 'react';
+import { TextInput, Button, TouchableOpacity, TouchableWithoutFeedback, Keyboard, StyleSheet } from 'react-native';
+import { printToFileAsync } from 'expo-print';
+import { shareAsync } from 'expo-sharing';
 
+import braille from '@/components/braille';
+
+/*
 export default function TabFourScreen() {
   return (
     <View style={styles.container}>
@@ -12,6 +19,77 @@ export default function TabFourScreen() {
     </View>
   );
 }
+*/
+
+export default function BrailleTranslation() {
+  const [inputText, setInputText] = useState('');
+  const [outputText, setOutputText] = useState('');
+  /*const translate = () => {
+    if (!inputText) {
+      setOutputText('');
+      return;
+    }
+  }
+*/
+
+const textToBraille = () => {
+  const tempText = inputText;
+  //setInputText(outputText);
+  setOutputText(braille.toBraille(tempText));
+};
+
+const html = `
+<html>
+    <body>
+   <p> ${outputText} </p>
+  </body>
+</html>
+`;
+
+let generatePDF = async() => {
+  const file = await printToFileAsync({
+    html: html,
+    base64: false
+  });
+
+  await shareAsync(file.uri);
+};
+
+return (
+  
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    <View style={styles.container}>
+    <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <TextInput
+        placeholder="Enter text here..."
+        value={inputText}
+        onChangeText={(text) => setInputText(text)}
+        multiline={true}
+        numberOfLines={(4)}
+        />
+
+    <TextInput
+        placeholder="Translation:"
+        value={outputText}
+        multiline={true}
+        numberOfLines={4}
+        />
+
+        <Button 
+          title = "Translate"
+        onPress={textToBraille}>
+        </Button>
+
+    <Button
+      title = "Generate PDF"
+      onPress={generatePDF}>
+      </Button>
+
+  </View>
+  </TouchableWithoutFeedback>
+);
+
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -29,3 +107,5 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
+
