821eb54110162f6c67f4e7d6ed1f0065d3e15b56
Precious Nwachokor <PreciousNwachokor@my.unt.edu>
2025-04-29 01:24:14 -0500

    Expo Go QR code should be fixed

--- Chemorization-App/app.config.js
+++ Chemorization-App/app.config.js
@@ -3,13 +3,12 @@ import 'dotenv/config';
 export default {
   expo: {
     name: 'Chemorization-App',
-    slug: 'Chemorization-App',
+    slug: 'chemorization-app',
+    owner: 'pcnwachokor',
     version: '1.0.0',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
-    scheme: 'myapp',
     userInterfaceStyle: 'automatic',
-    newArchEnabled: true,
     splash: {
       image: './assets/images/splash-icon.png',
       resizeMode: 'contain',
@@ -17,9 +16,10 @@ export default {
     },
     ios: {
       supportsTablet: true,
-      bundleIdentifier: 'com.anonymous.Chemorization-App',
+      bundleIdentifier: 'com.pcnwachokor.chemorizationapp',
     },
     android: {
+      package: 'com.pcnwachokor.chemorizationapp',
       adaptiveIcon: {
         foregroundImage: './assets/images/adaptive-icon.png',
         backgroundColor: '#ffffff',
@@ -34,6 +34,8 @@ export default {
     experiments: {
       typedRoutes: true,
     },
+    scheme: 'chemorizationapp',
+    newArchEnabled: true,
     extra: {
       OPENAI_API_KEY: process.env.OPENAI_API_KEY,
       ASSEMBLY_API_KEY: process.env.ASSEMBLY_API_KEY,
